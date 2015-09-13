b9c02df072e6f4bc2332d679b869ebd83af9739a
Samuil Gospodinov <samuil4@gmail.com>
2015-09-13 20:18:42 +0300

    adding base styles

--- EXERCISE/Exercise 4/app/templates/templates.js
+++ EXERCISE/Exercise 4/app/templates/templates.js
@@ -9,10 +9,30 @@ var app = app || {};
 
         layout: {
     		header: '<header id="template-header">Header</header>',
-    		footer: '<footer id="template-footer">Footer</footer>',
-    		content: '<section id="template-content">CONTENT</section>'
+    		footer: '<nav class="menu"><ul><li class="start"><a href="#" class="icon-smile"></a></li><li class="window-tab"><a href="#" class="icon-browser"></a></li></ul></nav>',
+    		content: ''
         },
 
-        windowTemplate: '<div>Window :) yeeeh</div>'
+        windowTemplate: ['<section class="window" id="test">',
+            '<header class="header">',
+                '<div class="window-actions">',
+                    '<span class="action icon-dash"></span>',
+                    '<span class="action icon-popup"></span>',
+                    '<span class="action icon-delete-circle"></span>',
+                '</div>',
+                '<h2>Title</h2>',
+            '</header>',
+            '<div class="pure-g">',
+                '<aside class="pure-u-1 pure-u-md-1-5 sidebar">',
+                   'Additional actions',
+                '</aside>',
+                '<main class="pure-u-1 pure-u-md-4-5 content">',
+                    'Main content',
+                '</main>',
+            '</div>',
+            '<footer class="footer">',
+                '<strong> Status bar :) </strong>',
+            '</footer>',
+        '</section>']
 	};
 })();
